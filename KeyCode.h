c8ad06b24c705db9c4110af749a45ab806f17b40
p.luneau <paul.luneau@mail.com>
2024-09-18 15:51:55 +0200

    Put the key codes in a glossary

---
+++ KeyCode.h
@@ -0,0 +1,12 @@
+#pragma once
+class KeyCode
+{
+public:
+	static const int ARROW_LEFT = 37;
+	static const int ARROW_UP = 38;
+	static const int ARROW_RIGHT = 39;
+	static const int ARROW_DOWN = 40;
+
+	static const int KEY_R = 82;
+};
+
